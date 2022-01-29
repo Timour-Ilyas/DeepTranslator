8e3e4cc1e0e67db4b8d8c0927c21653b22dd06a7
XiroD <xirodan87@gmail.com>
2022-01-29 19:02:52 +0100

    Inserimento logo applicazione

--- app/src/main/java/com/example/deeptranslator/MainActivity.java
+++ app/src/main/java/com/example/deeptranslator/MainActivity.java
@@ -16,21 +16,16 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import android.speech.tts.TextToSpeech;
-
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
